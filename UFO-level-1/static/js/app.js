ea96b72d37834eb6d39287d6f5cedb6e90010901
Michelle Brucato <MichelleRBrucato@gmail.com>
2020-03-11 23:12:43 -0500

    update app.py and add backround image to css.

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,25 @@
-// from data.js
-const tableData = data;
+const input = d3.select("input")
+const button = d3.select("button")
 
-// YOUR CODE HERE!
+const tbody = d3.select("tbody")
+
+const handler = function(){
+
+  tbody.html("")
+
+  let filterCond = input.property("value")
+
+  data.filter(dataRow => dataRow.datetime === filterCond).forEach(dataRow => {
+    let row = tbody.append("tr")
+    row.append("td").text(dataRow.datetime)
+    row.append("td").text(dataRow.city)
+    row.append("td").text(dataRow.state)
+    row.append("td").text(dataRow.country)
+    row.append("td").text(dataRow.shape)
+    row.append("td").text(dataRow.durationMinutes)
+    row.append("td").text(dataRow.comments)
+  })
+}
+input.on("change", handler)
+button.on("click", handler)
+
