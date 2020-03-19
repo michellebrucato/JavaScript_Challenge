d684fe8414011054fae658d34b485f5bd8378fa7
Michelle Brucato <MichelleRBrucato@gmail.com>
2020-03-19 18:15:01 -0500

    fix app.js file to show data on homepage so user is aware of dates to filter by.

--- static/js/app.js
+++ static/js/app.js
@@ -1,14 +1,24 @@
 const input = d3.select("input")
 const button = d3.select("button")
-
 const tbody = d3.select("tbody")
 
+// Display the full table at home page
+data.filter(dataRow => dataRow).forEach(dataRow => {
+  let row = tbody.append("tr")
+  row.append("td").text(dataRow.datetime)
+  row.append("td").text(dataRow.city)
+  row.append("td").text(dataRow.state)
+  row.append("td").text(dataRow.country)
+  row.append("td").text(dataRow.shape)
+  row.append("td").text(dataRow.durationMinutes)
+  row.append("td").text(dataRow.comments)
+})
+
+// Assign function to handler
 const handler = function(){
 
   tbody.html("")
-
   let filterCond = input.property("value")
-
   data.filter(dataRow => dataRow.datetime === filterCond).forEach(dataRow => {
     let row = tbody.append("tr")
     row.append("td").text(dataRow.datetime)
@@ -20,6 +30,6 @@ const handler = function(){
     row.append("td").text(dataRow.comments)
   })
 }
+
 input.on("change", handler)
 button.on("click", handler)
-
